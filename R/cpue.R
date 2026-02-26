05cdd80bd4cd19ff3caa5c8ebe201e93a09e27e7
Noel Swain <Noel.Swain@dfo-mpo.gc.ca>
2026-02-26 10:05:54 -0800

    setting up S3 class and methods and constructor functions

--- R/cpue.R
+++ R/cpue.R
@@ -16,27 +16,71 @@
 #' @examples
 #' cpue(100, 10)
 #' cpue(c(100, 200), c(10, 20), method = "log")
-cpue <- function(catch,
-                 effort,
-                 gear_factor = 1,
-                 method = c("ratio", "log"),
-                 verbose = getOption("fishr.verbose", FALSE)
-                 ) {
+cpue <- function(
+    catch,
+    effort,
+    gear_factor = 1,
+    method = c("ratio", "log"),
+    verbose = getOption("fishr.verbose", FALSE)
+) {
   method <- match.arg(method)
 
   validate_numeric_inputs(catch = catch, effort = effort)
 
-  # side effect: prints a message
   if (verbose) {
     message("Processing ", length(catch), " records using ", method, " method")
   }
 
-  # Pure calculation
-raw_cpue <- switch(
-  method,
-  ratio = catch / effort,
-  log = log(catch / effort)
-)
+  raw_cpue <- switch(
+    method,
+    ratio = catch / effort,
+    log = log(catch / effort)
+  )
 
-  raw_cpue * gear_factor
+  result <- raw_cpue * gear_factor
+  attr(result, "gear_factor") <- gear_factor
+  attr(result, "n_records") <- length(catch)
+  attr(result, "method") <- method
+  class(result) <- "cpue_result"
+  result
+}
+
+
+
+#' @export
+print.cpue_result <- function(x, ...) {
+  cat("CPUE Result\n")
+  cat("Records:     ", attr(x, "n_records"), "\n")
+  cat("Method:      ", attr(x, "method"), "\n")
+  cat("Gear factor: ", attr(x, "gear_factor"), "\n")
+  cat("Values:      ", round(x, 2), "\n")
+  invisible(x)
+}
+
+
+#' @export
+summary.cpue_result <- function(object, ...) {
+  cat("Survey Result Summary\n")
+  cat("---------------------\n")
+  cat("Method:      ", attr(object, "method"), "\n")
+  cat("Records:     ", attr(object, "n_records"), "\n")
+  cat("Gear factor: ", attr(object, "gear_factor"), "\n")
+  cat("Mean CPUE:   ", round(mean(object), 2), "\n")
+  cat("Median CPUE: ", round(stats::median(object), 2), "\n")
+  cat("SD CPUE:     ", round(stats::sd(object), 2), "\n")
+  invisible(object)
+}
+
+
+#' @export
+plot.cpue_result <- function(x, ...) {
+  plot(
+    seq_along(x),
+    x,
+    type = "b",
+    xlab = "Record",
+    ylab = "CPUE",
+    main = paste("CPUE -", attr(x, "method"), "method"),
+    ...
+  )
 }
